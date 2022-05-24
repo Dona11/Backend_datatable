85a723568efe928e4d5926b60bdcbd4a6830bcf8
Dona11 <matti.dona03@gmail.com>
2022-05-24 23:31:17 +0200

    secondo commit

--- index.php
+++ index.php
@@ -6,60 +6,28 @@
     header("Access-Control-Allow-Headers: *");
     header("Access-Control-Allow-Methods: *");
     $ar = array();
-    $page = @$_POST['start'] ?? 0;
-    $size = @$_POST['length'] ?? 10;
+    $page = $_POST['start'];
+    $size = $_POST['length'];
     $search = $_POST['search'];
+    $order = $_POST['order'];
     $tot = countRow();
     $URL = "http://127.0.0.1:8080/";
     $method = $_SERVER['REQUEST_METHOD'];
     $last = floor($tot / $size);
 
-    
-
-    
-
     if($method == "POST"){
-        //var_dump($search);
-        if($search ){
-            $ar['data'] = getPage($page, $size);
+        if($search['value'] == ""){
+            $ar['data'] = getPage($page, $size, $order);
             $ar['recordsFiltered'] = $tot;
             $ar['recordsTotal'] = $tot;
             echo json_encode($ar);
+        }else{
+            $totSearch = countRowSearch($search['value']);
+            $ar['data'] = search($page, $size, $search['value'], $order);
+            $ar['recordsFiltered'] = $totSearch;
+            $ar['recordsTotal'] = $totSearch;
+            echo json_encode($ar);
         }
-        
-    }/*else if($method == "POST"){
-        $data = json_decode(file_get_contents('php://input'), true);
-        postEmployee($data["birth_date"], $data['first_name'], $data['last_name'], $data['gender']);
-        echo json_encode($data);
-    }*/else if($method == "DELETE"){
-        deleteEmployee($_GET['id']);
-        echo json_encode($ar);
-    }else if($method == "PUT"){
-        $data = json_decode(file_get_contents('php://input'), true);
-        editEmployee($_GET['id'], $data["birth_date"], $data['first_name'], $data['last_name'], $data['gender']);
-        echo json_encode($data);
-    }
-
-    function href($url, $page, $size){
-
-        $href = $url . "?page=" . $page . "&size=" . $size;
-        return $href;
-    }
-
-    function links($page, $size, $last, $URL){
-        $links = array();
-        $links['first']['href'] = href($URL, 0, $size);
-
-        if($page > 0){
-            $links['prev']['href'] = href($URL, ($page - 1), $size);
-        }
-        if($page < $last){
-
-            $links['next']['href'] = href($URL, ($page + 1), $size);
-        }
-        $links['last']['href'] = href($URL, $last, $size);
-
-        return $links;
     }
 
 ?>
--- pages/dbLayer.php
+++ pages/dbLayer.php
@@ -1,20 +1,76 @@
 <?php
 
     function countRow(){
-
         require ('data.php');
         $query = "SELECT count(*) as n FROM employees";
         $result = mysqli_query($connessione, $query)
             or die ("Query fallita " . mysqli_error($connessione) . " " . mysqli_errno($connessione));
         $row = mysqli_fetch_array ($result);
         return $row[0];
-
     }
 
-    function getPage($first, $lenght){
-
+    function countRowSearch($string){
         require ('data.php');
-        $query = "SELECT * FROM employees ORDER BY id LIMIT $first, $lenght";
+        $query = "SELECT count(*) as n FROM employees WHERE (last_name LIKE '%$string%' OR id  LIKE '%$string%' OR birth_date LIKE '%$string%' OR first_name LIKE '%$string%')";
+        $result = mysqli_query($connessione, $query)
+            or die ("Query fallita " . mysqli_error($connessione) . " " . mysqli_errno($connessione));
+        $row = mysqli_fetch_array ($result);
+        return $row[0];
+    }
+
+    function getPage($first, $lenght, $order){
+        require ('data.php');
+
+        if($order[0]['column'] == 0){
+            $colonna = "id";
+        }else if($order[0]['column'] == 1){
+            $colonna = "birth_date";
+        }else if($order[0]['column'] == 2){
+            $colonna = "first_name";
+        }else if($order[0]['column'] == 3){
+            $colonna = "last_name";
+        }else if($order[0]['column'] == 4){
+            $colonna = "gender";
+        }
+
+        if($order[0]['dir'] == 'asc'){
+            $verso = "ASC";
+        }else{
+            $verso = "DESC";
+        }
+
+            $query = "SELECT * FROM employees ORDER BY $colonna $verso LIMIT $first, $lenght";
+            $result = mysqli_query($connessione, $query)
+                or die ("Query fallita " . mysqli_error($connessione) . " " . mysqli_errno($connessione));
+            $rows = array();
+            while ($row = $result -> fetch_assoc()) {
+                $rows[] = $row;
+            }
+            return $rows;
+    }
+
+    function search($first, $lenght, $string, $order){
+        require ('data.php');
+
+        if($order[0]['column'] == 0){
+            $colonna = "id";
+        }else if($order[0]['column'] == 1){
+            $colonna = "birth_date";
+        }else if($order[0]['column'] == 2){
+            $colonna = "first_name";
+        }else if($order[0]['column'] == 3){
+            $colonna = "last_name";
+        }else if($order[0]['column'] == 4){
+            $colonna = "gender";
+        }
+
+        if($order[0]['dir'] == 'asc'){
+            $verso = "ASC";
+        }else{
+            $verso = "DESC";
+        }
+
+        $query = "SELECT * FROM employees WHERE (last_name LIKE '%$string%' OR id  LIKE '%$string%' OR birth_date LIKE '%$string%' OR first_name LIKE '%$string%') ORDER BY $colonna $verso LIMIT $first, $lenght";
         $result = mysqli_query($connessione, $query)
             or die ("Query fallita " . mysqli_error($connessione) . " " . mysqli_errno($connessione));
         $rows = array();
@@ -22,24 +78,6 @@
             $rows[] = $row;
         }
         return $rows;
-    }
-
-    function postEmployee($birth_date, $first_name, $last_name, $gender){
-        require ('data.php');
-        $query = "INSERT INTO employees (birth_date, first_name, last_name, gender, hire_date) VALUES ('$birth_date', '$first_name', '$last_name', '$gender', null)";
-        $connessione -> query($query);
-    }
-
-    function deleteEmployee($id){
-        require ('data.php');
-        $query = "DELETE FROM employees WHERE employees.id = $id";
-        $connessione -> query($query);
-    }
-
-    function editEmployee($id, $birth_date, $first_name, $last_name, $gender){
-        require ('data.php');
-        $query = "UPDATE employees SET birth_date = '$birth_date', first_name = '$first_name', last_name = '$last_name', gender = '$gender' WHERE id = $id";
-        $connessione -> query($query);
     }
 
 ?>
